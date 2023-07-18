568f0fe64709ba6baaa5c47ea8a57aafa0f4423e
Pranshu Adhwaryu <pranshuadhwaryu19@gmail.com>
2023-07-18 23:42:59 +0530

    GitUserSearch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState(null);
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchUserData();
+  };
+
+  const fetchUserData = () => {
+    axios
+      .get(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        setUser(null);
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub Username"
+          value={username}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {user ? (
+        <div className="user-info">
+          <img src={user.avatar_url} alt="User Avatar" />
+          <h2>{user.name}</h2>
+
+          <table>
+            <tbody>
+              <tr>
+                <td>Location:</td>
+                <td>{user.location || '-'}</td>
+              </tr>
+              <tr>
+                <td>Total Repos:</td>
+                <td>{user.public_repos || '-'}</td>
+              </tr>
+              <tr>
+                <td>Bio:</td>
+                <td>{user.bio || '-'}</td>
+              </tr>
+              <tr>
+                <td>Followers:</td>
+                <td>{user.followers || '-'}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p>No user found. Please enter a valid GitHub username.</p>
+      )}
     </div>
   );
 }
